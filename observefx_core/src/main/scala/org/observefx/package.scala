b43cbab3d0efa06c5ca0d5328c5b403ccbf70fae
John Ky <newhoggy@gmail.com>
2013-02-24 02:21:00 +1100

    Simplify types.

--- observefx_core/src/main/scala/org/observefx/package.scala
+++ observefx_core/src/main/scala/org/observefx/package.scala
@@ -8,22 +8,9 @@ import scala.collection.mutable.Undoable
 import scala.collection.mutable.Buffer
 
 package object observefx {
-  type ObservableSeq[A] = Publisher[Message[A]]
-  
-  class ObservableSeqOnBuffer[A](in: ObservableBuffer[A]) extends ObservableSeq[A] {
-    def apply(idx: Int): A = in.apply(idx)
-    def length: Int = in.length
-    def iterator: Iterator[A] = in.iterator
-    
-    in.subscribe(new Subscriber[Message[A], ObservableBuffer[A]] {
-      override def notify(pub: ObservableBuffer[A], event: Message[A]): Unit = {
-        publish(event)
-      }
-    })
-  }
+  type ObservableSeq[A] = Publisher[Message[A] with Undoable]
   
   implicit class RichObservableBuffer[A](val in: Buffer[A] with ObservableBuffer[A]) extends AnyVal {
-    def immutable: ObservableSeq[A] = new ObservableSeqOnBuffer(in)
-    def observableSeq: Seq[A] with Publisher[Message[A] with Undoable] = in
+    def observableSeq: Seq[A] with ObservableSeq[A] = in
   }
 }
