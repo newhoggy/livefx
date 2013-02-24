61f912a7bda0ef0309882e6d7e67838c56534acb
John Ky <newhoggy@gmail.com>
2013-02-25 10:52:15 +1100

    Test case.

--- observefx_core/src/test/scala/org/observfx/TestObservableSeq.scala
+++ observefx_core/src/test/scala/org/observfx/TestObservableSeq.scala
@@ -4,6 +4,7 @@ import org.junit.Test
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.ObservableBuffer
 import org.observefx._
+import org.junit.Assert
 
 class TestObservableSeq {
   @Test
@@ -11,12 +12,14 @@ class TestObservableSeq {
     val buffer = new ArrayBuffer[Int] with ObservableBuffer[Int]
     val stringSeq = buffer.liveMap { value => "[" + value + "]" }
     buffer.insert(0, 0)
-    println(stringSeq)
+    Assert.assertTrue(stringSeq.sameElements(List("[0]")))
     buffer.append(1)
-    println(stringSeq)
+    Assert.assertTrue(stringSeq.sameElements(List("[0]", "[1]")))
     buffer.prepend(2)
-    println(stringSeq)
+    Assert.assertTrue(stringSeq.sameElements(List("[2]", "[0]", "[1]")))
     buffer.insertAll(1, List(3, 4, 5))
-    println(stringSeq)
+    Assert.assertTrue(stringSeq.sameElements(List("[2]", "[3]", "[4]", "[5]", "[0]", "[1]")))
+    buffer.clear()
+    Assert.assertTrue(stringSeq.sameElements(List()))
   }
 }
