0caa43b593364c3ed46329e9ecbdb3b7ddf23ec4
John Ky <newhoggy@gmail.com>
2013-03-04 10:30:47 +1100

    New TestAll test suite.

---
+++ observefx_core/src/test/scala/org/livefx/TestAll.scala
@@ -0,0 +1,8 @@
+package org.livefx
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(classOf[Suite])
+@Suite.SuiteClasses(Array(classOf[TestObservableSeq]))
+class TestAll
