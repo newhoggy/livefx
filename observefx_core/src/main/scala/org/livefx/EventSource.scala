4699a492a3ad4237b7d5c4a21078e81bba65f5b7
John Ky <newhoggy@gmail.com>
2013-03-04 10:42:34 +1100

    Hold onto subscribers weakly.

--- observefx_core/src/main/scala/org/livefx/EventSource.scala
+++ observefx_core/src/main/scala/org/livefx/EventSource.scala
@@ -3,15 +3,15 @@ package org.livefx
 import scala.collection.mutable.WeakHashMap
 
 class EventSource[P, E](val publisher: P) extends Events[P, E] with EventSink[E] {
-  private val subscribers = new HashSet[(P, E) => Unit]
+  private lazy val subscribers = new WeakHashMap[(P, E) => Unit, Unit]
 
   override def subscribe(subscriber: (P, E) => Unit): Disposable = new Disposable {
-    subscribers += subscriber
+    subscribers += (subscriber -> Unit)
     
     override def dispose(): Unit = subscribers -= subscriber
   }
 
   override def unsubscribe(subscriber: (P, E) => Unit): Unit = { subscribers -= subscriber }
 
-  override def publish(event: E): Unit = subscribers.foreach(subscriber => subscriber(publisher, event))
+  override def publish(event: E): Unit = subscribers.foreach { entry => entry._1(publisher, event) }
 }
