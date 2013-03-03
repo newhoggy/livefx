971ee016c4eefeb81234491da5e79e71901927a9
John Ky <newhoggy@gmail.com>
2013-03-04 10:38:06 +1100

    New testChangeSubscriber.

--- observefx_core/src/test/scala/org/livefx/TestSimpleLiveValue.scala
+++ observefx_core/src/test/scala/org/livefx/TestSimpleLiveValue.scala
@@ -2,6 +2,9 @@ package org.livefx
 
 import org.junit.Test
 import org.junit.Assert
+import org.livefx.script.Change
+import org.livefx.script.Update
+import org.livefx.script.NoLo
 
 class TestSimpleLiveValue {
   @Test
@@ -27,5 +30,28 @@ class TestSimpleLiveValue {
     Assert.assertEquals(1, spoilCount)
     Assert.assertEquals(3, liveValue.value)
     Assert.assertEquals(false, liveValue.spoiled)
+  }
+
+  @Test
+  def testChangeSubscriber(): Unit = {
+    var changes = List[Change[Int]]()
+    val liveValue = new SimpleLiveValue[Int](0)
+    
+    val subscription = liveValue.changes.subscribe((_, change) => changes = change::changes)
+    Assert.assertEquals(0, liveValue.value)
+    Assert.assertEquals(Nil, changes)
+
+    liveValue.value = 11
+    Assert.assertEquals(11, liveValue.value)
+    Assert.assertEquals(List(Update(NoLo, 0, 11)), changes)
+
+    liveValue.value = 12
+    Assert.assertEquals(12, liveValue.value)
+    Assert.assertEquals(List(Update(NoLo, 11, 12), Update(NoLo, 0, 11)), changes)
+    
+    subscription.dispose
+    liveValue.value = 13
+    Assert.assertEquals(13, liveValue.value)
+    Assert.assertEquals(List(Update(NoLo, 11, 12), Update(NoLo, 0, 11)), changes)
   }
 }
