13920b9f6e3b8bd851ec15df4841c9797a1b4e62
John Ky <newhoggy@gmail.com>
2013-03-07 10:51:54 +1100

    LiveOrdering trait.

---
+++ observefx_core/src/main/scala/org/livefx/LiveOrdering.scala
@@ -0,0 +1,69 @@
+package org.livefx
+
+trait LiveOrdering[T] extends LivePartialOrdering[T] with Serializable {
+  outer =>
+
+  /** Returns whether a comparison between `x` and `y` is defined, and if so
+    * the result of `compare(x, y)`.
+    */
+  def tryCompare(x: LiveValue[T], y: LiveValue[T]) = compare(x, y).map(Some(_))
+
+  def compare(x: T, y: T): Int
+  
+  /** Returns an integer whose sign communicates how x compares to y.
+   *
+   * The result sign has the following meaning:
+   *
+   *  - negative if x < y
+   *  - positive if x > y
+   *  - zero otherwise (if x == y)
+   */
+  def compare(x: LiveValue[T], y: LiveValue[T]): LiveValue[Int] = for (xv <- x; yv <- y) yield compare(xv, yv)
+
+  /** Return true if `x` <= `y` in the ordering. */
+  override def lteq(x: LiveValue[T], y: LiveValue[T]): LiveValue[Boolean] = compare(x, y).map(_ <= 0)
+
+  /** Return true if `x` >= `y` in the ordering. */
+  override def gteq(x: LiveValue[T], y: LiveValue[T]): LiveValue[Boolean] = compare(x, y).map(_ >= 0)
+
+  /** Return true if `x` < `y` in the ordering. */
+  override def lt(x: LiveValue[T], y: LiveValue[T]): LiveValue[Boolean] = compare(x, y).map(_ < 0)
+
+  /** Return true if `x` > `y` in the ordering. */
+  override def gt(x: LiveValue[T], y: LiveValue[T]): LiveValue[Boolean] = compare(x, y).map(_ > 0)
+
+  /** Return true if `x` == `y` in the ordering. */
+  override def equiv(x: LiveValue[T], y: LiveValue[T]): LiveValue[Boolean] = compare(x, y).map(_ == 0)
+
+  /** Return `x` if `x` >= `y`, otherwise `y`. */
+  def max(x: LiveValue[T], y: LiveValue[T]): LiveValue[T] = for (xv <- x; yv <- y) yield if (compare(xv, yv) >= 0) xv else yv 
+
+  /** Return `x` if `x` <= `y`, otherwise `y`. */
+  def min(x: LiveValue[T], y: LiveValue[T]): LiveValue[T] = for (xv <- x; yv <- y) yield if (compare(xv, yv) <= 0) xv else yv
+
+  /** Return the opposite ordering of this one. */
+  override def reverse: LiveOrdering[T] = new LiveOrdering[T] {
+    override def reverse = outer
+    def compare(x: T, y: T) = outer.compare(y, x)
+  }
+
+  def on[U](f: U => T): LiveOrdering[U] = new LiveOrdering[U] {
+    def compare(x: U, y: U) = outer.compare(f(x), f(y))
+  }
+
+  /** This inner class defines comparison operators available for `T`. */
+  class Ops(lhs: LiveValue[T]) {
+    def <(rhs: LiveValue[T]) = lt(lhs, rhs)
+    def <=(rhs: LiveValue[T]) = lteq(lhs, rhs)
+    def >(rhs: LiveValue[T]) = gt(lhs, rhs)
+    def >=(rhs: LiveValue[T]) = gteq(lhs, rhs)
+    def equiv(rhs: LiveValue[T]) = LiveOrdering.this.equiv(lhs, rhs)
+    def max(rhs: LiveValue[T]): LiveValue[T] = LiveOrdering.this.max(lhs, rhs)
+    def min(rhs: LiveValue[T]): LiveValue[T] = LiveOrdering.this.min(lhs, rhs)
+  }
+
+  /** This implicit method augments `T` with the comparison operators defined
+    * in `scala.math.Ordering.Ops`.
+    */
+  implicit def mkOrderingOps(lhs: LiveValue[T]): Ops = new Ops(lhs)
+}
--- observefx_core/src/main/scala/org/livefx/package.scala
+++ observefx_core/src/main/scala/org/livefx/package.scala
@@ -11,7 +11,13 @@ package object livefx {
   type Map[A, B] = mutable.Map[A, B]
   type Set[A] = mutable.Set[A]
   
-  implicit class RichLiveBuffer[A](val in: Buffer[A] with LiveBuffer[A]) {
-    def asSeq: Seq[A] with LiveSeq[A] = in
+  implicit class RichLiveBuffer[A](val self: Buffer[A] with LiveBuffer[A]) {
+    def asSeq: Seq[A] with LiveSeq[A] = self
+  }
+  
+  implicit class RichLiveValueBoolean(val self: LiveValue[Boolean]) {
+    def &&(that: LiveValue[Boolean]): LiveValue[Boolean] =  for (l <- self; r <- that) yield l && r
+    def ||(that: LiveValue[Boolean]): LiveValue[Boolean] =  for (l <- self; r <- that) yield l || r
+    def unary_!(): LiveValue[Boolean] =  self.map(!_)
   }
 }
