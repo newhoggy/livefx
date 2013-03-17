4e5e732861c08d55e31d9e6996975da2818fae2a
John Ky <newhoggy@gmail.com>
2013-03-18 10:40:19 +1100

    Restructure for macros.

--- project/Build.scala
+++ project/Build.scala
@@ -2,10 +2,10 @@ import sbt._
 import Keys._
 
 object XSDK extends Build {
-  lazy val root = Project(id = "observesfx", base = file(".")) aggregate(livefx_core, livefx_coretest)
+  lazy val root = Project(id = "observesfx", base = file(".")) aggregate(livefx_core, livefx_bindings)
 
   lazy val livefx_core = Project(id = "livefx_core", base = file("livefx_core"))
 
-  lazy val livefx_coretest = Project(id = "livefx_coretest", base = file("livefx_coretest")) dependsOn(livefx_core)
+  lazy val livefx_bindings = Project(id = "livefx_bindings", base = file("livefx_bindings")) dependsOn(livefx_core)
 }
 
