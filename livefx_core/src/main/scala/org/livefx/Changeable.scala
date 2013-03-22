7637c77965a7b07c070aaa3051480d931692cad9
John Ky <newhoggy@gmail.com>
2013-03-22 22:14:13 +1100

    Make LiveValue type argument covariant.

--- livefx_core/src/main/scala/org/livefx/Changeable.scala
+++ livefx_core/src/main/scala/org/livefx/Changeable.scala
@@ -2,7 +2,7 @@ package org.livefx
 
 import org.livefx.script.Change
 
-trait Changeable[A, M <: Change[A]] extends Publisher {
+trait Changeable[+A, +M <: Change[A]] extends Publisher {
   type Pub <: Changeable[A, M]
   type ChangeableMessage = M
   
