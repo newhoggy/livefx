477814420149dd76d44eb1123993cd06617ead4d
John Ky <newhoggy@gmail.com>
2013-04-19 10:17:10 +1000

    New size method.

--- livefx_core/src/main/scala/org/livefx/gap/Trees.scala
+++ livefx_core/src/main/scala/org/livefx/gap/Trees.scala
@@ -9,6 +9,7 @@ abstract class Trees[+A] {
   def isEmpty: Boolean
   def ::[B >: A](head: Tree[B]): Trees[B] = TreesCons(head, this)
   def treeCount: Int
+  def size: Int
   def trees: List[Tree[A]]
 
   def drop(n: Int): Trees[A]
@@ -19,6 +20,7 @@ abstract class Trees[+A] {
 final case class TreesCons[+A](head: Tree[A], tail: Trees[A]) extends Trees[A] {
   override def isEmpty: Boolean = false
   override val treeCount: Int = tail.treeCount + 1
+  override val size: Int = head.size + tail.size
   override def trees: List[Tree[A]] = head::tail.trees
 
   override def drop(n: Int): Trees[A] = {
@@ -37,6 +39,7 @@ final object TreesNil extends Trees[Nothing] {
   override def tail: Trees[Nothing] = throw new UnsupportedOperationException
   override def isEmpty: Boolean = true
   override def treeCount: Int = 0
+  override def size: Int = 0
   override def trees: List[Tree[Nothing]] = Nil
   override def drop(n: Int): Trees[Nothing] = if (n == 0) this else throw new IndexOutOfBoundsException
   override def take(n: Int): Trees[Nothing] = if (n == 0) this else throw new IndexOutOfBoundsException
