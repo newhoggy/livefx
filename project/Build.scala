ebe38f5289cef6cf128e5bfa029e0805df8b0432
John Ky <newhoggy@gmail.com>
2013-04-26 21:21:39 +1000

    Remove livefx_bindings project.

--- project/Build.scala
+++ project/Build.scala
@@ -11,10 +11,8 @@ object XSDK extends Build {
 
   lazy val commonSettings = Defaults.defaultSettings ++ buildSettings
 
-  lazy val root = Project(id = "observesfx", base = file(".")).aggregate(livefx_core, livefx_bindings).settings(commonSettings: _*)
+  lazy val root = Project(id = "observesfx", base = file(".")).aggregate(livefx_core).settings(commonSettings: _*)
 
   lazy val livefx_core = Project(id = "livefx_core", base = file("livefx_core")).settings(commonSettings: _*)
-
-//  lazy val livefx_bindings = Project(id = "livefx_bindings", base = file("livefx_bindings")).settings(commonSettings: _*).dependsOn(livefx_core)
 }
 
