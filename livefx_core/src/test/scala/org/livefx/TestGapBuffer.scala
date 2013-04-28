8fcba366a5c34d37165839b4566368f3f230b3b5
John Ky <newhoggy@gmail.com>
2013-04-29 02:25:37 +1000

    New test case.

--- livefx_core/src/test/scala/org/livefx/TestGapBuffer.scala
+++ livefx_core/src/test/scala/org/livefx/TestGapBuffer.scala
@@ -2,8 +2,42 @@ package org.livefx
 
 import org.junit.Test
 import org.junit.Assert
-import org.livefx.gap.Branch
-import org.livefx.gap.Leaf
+import org.livefx.gap._
 
 class TestGapBuffer {
+  @Test
+  def test1(): Unit = {
+    val random = new scala.util.Random(0)
+    var list: ArrayBuffer[Int] = new ArrayBuffer[Int]()
+    var tree: Tree[Int] = Leaf0
+
+    for (i <- 0 to 100) {
+      random.nextInt(2) match {
+        case 0 =>
+          val index = random.nextInt(list.size + 1)
+          println(s"--> (${list.size})insert($index, $i)")
+          list.insert(index, i)
+          tree = tree.insert(index, i)
+        case 1 =>
+//          if (list.size > 0) {
+//            val index = random.nextInt(list.size)
+//            println(s"--> (${list.size})update($index, $i)")
+//            list(index) = i
+//            tree = tree.update(index, i)
+//          }
+      }
+
+      println(s"--> tree: $tree")
+      Assert.assertEquals(list.toList, tree.toList(Nil))
+    }
+
+//    for (i <- 0 to list.size - 1) {
+//      val index = random.nextInt(list.size)
+//      println(s"--> [$i] remove($index)")
+//      list.remove(index)
+//      tree = tree.delete(index)
+//      println(s"--> tree: $tree")
+//      Assert.assertEquals(list.toList, Tree.iterator(tree).toList)
+//    }
+  }
 }
