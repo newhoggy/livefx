1e779f5c4631f2b7a78cd703e0421b91a5b3a4ea
John Ky <newhoggy@gmail.com>
2013-05-10 16:13:01 +1000

    New volumeIndex method.

--- livefx_core/src/main/scala/org/livefx/volume/Root.scala
+++ livefx_core/src/main/scala/org/livefx/volume/Root.scala
@@ -1,6 +1,7 @@
 package org.livefx.volume
 
-import scalaz.Monoid
+import scalaz._
+import Scalaz._
 
 final case class Root[+A <% M, M: Monoid](child: Tree[A, M]) {
   final def size: Int = child.size
@@ -13,6 +14,105 @@ final case class Root[+A <% M, M: Monoid](child: Tree[A, M]) {
     case Leaf4(a, b, c, d) => Branch2(Leaf2(a, b), Leaf2(c, d))
     case newChild => newChild
   })
+  
+  final def volumeIndex(volume: M)(implicit ordering: scala.math.Ordering[M]): Int = {
+    def volumeIndex(tree: Tree[A, M], leftVolume: M): Int = {
+      import Numeric.Implicits._
+      import ordering._
+      
+      tree match {
+        case Leaf0() => {
+          if (leftVolume == volume) 0
+          else throw new IndexOutOfBoundsException
+        }
+        case Leaf1(a) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| (a: M)
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) 0
+          else throw new IndexOutOfBoundsException
+        }
+        case Leaf2(a, b) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| (a: M)
+          lazy val volume2c = volume2b |+| (b: M)
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) 0
+          else if (volume2c < volume) 1
+          else throw new IndexOutOfBoundsException
+        }
+        case Leaf3(a, b, c) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| (a: M)
+          lazy val volume2c = volume2b |+| (b: M)
+          lazy val volume2d = volume2c |+| (c: M)
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) 0
+          else if (volume2c < volume) 1
+          else if (volume2d < volume) 2
+          else throw new IndexOutOfBoundsException
+        }
+        case Leaf4(a, b, c, d) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| (a: M)
+          lazy val volume2c = volume2b |+| (b: M)
+          lazy val volume2d = volume2c |+| (c: M)
+          lazy val volume2e = volume2d |+| (d: M)
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) 0
+          else if (volume2c < volume) 1
+          else if (volume2d < volume) 2
+          else if (volume2e < volume) 3
+          else throw new IndexOutOfBoundsException
+        }
+        case Branch1(a) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| a.volume
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) volumeIndex(a, volume2a) 
+          else throw new IndexOutOfBoundsException
+        }
+        case Branch2(a, b) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| a.volume
+          lazy val volume2c = volume2b |+| b.volume
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) volumeIndex(a, volume2a)
+          else if (volume2c < volume) volumeIndex(b, volume2b)
+          else throw new IndexOutOfBoundsException
+        }
+        case Branch3(a, b, c) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| a.volume
+          lazy val volume2c = volume2b |+| b.volume
+          lazy val volume2d = volume2c |+| c.volume
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) volumeIndex(a, volume2a)
+          else if (volume2c < volume) volumeIndex(b, volume2b)
+          else if (volume2d < volume) volumeIndex(c, volume2c)
+          else throw new IndexOutOfBoundsException
+        }
+        case Branch4(a, b, c, d) => {
+          lazy val volume2a = leftVolume
+          lazy val volume2b = volume2a |+| a.volume
+          lazy val volume2c = volume2b |+| b.volume
+          lazy val volume2d = volume2c |+| c.volume
+          lazy val volume2e = volume2d |+| d.volume
+          if (volume2a < volume) throw new IndexOutOfBoundsException
+          else if (volume2b < volume) volumeIndex(a, volume2a)
+          else if (volume2c < volume) volumeIndex(b, volume2b)
+          else if (volume2d < volume) volumeIndex(c, volume2c)
+          else if (volume2e < volume) volumeIndex(d, volume2d)
+          else throw new IndexOutOfBoundsException
+        }
+        case _ => {
+          throw new IndexOutOfBoundsException
+        }
+      }
+    }
+
+    return volumeIndex(child, volume)
+  }
 
   final def update[B >: A <% M](index: Int, value: B): Root[B, M] = Root(child.update(index, value))
 
