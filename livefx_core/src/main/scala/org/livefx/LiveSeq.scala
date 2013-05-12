45d3de660197a8bd39450f887dd010e62031368a
John Ky <newhoggy@gmail.com>
2013-05-12 17:16:46 +1000

    Temporary code.

--- livefx_core/src/main/scala/org/livefx/LiveSeq.scala
+++ livefx_core/src/main/scala/org/livefx/LiveSeq.scala
@@ -10,4 +10,73 @@ trait LiveSeq[A] extends LiveValue[Tree[A]] {
   def value: Tree[A]
   
   def changes: Events[Pub, Change[A]]
+  
+//  def liveMap[B](f: A => B): LiveSeq[B] = {
+//    val outer = this
+//    new LiveSeq[B] {
+//      private final def target = this
+//      private val ref = outer.changes.subscribeWeak { (pub: OldLiveSeq[A], change: Change[A]) =>
+//        def process(pub: OldLiveSeq[A], change: Change[A]): Unit = {
+//          change match {
+//            case Remove(location, oldElem) => location match {
+//              case Start => target.remove(0)
+//              case End => target.remove(target.size - 1)
+//              case Index(index) => target.remove(index)
+//              case NoLo => assert(false)
+//            }
+//            case Include(location, newElem) => location match {
+//              case Start => target.prepend(f(newElem))
+//              case End => target.append(f(newElem))
+//              case Index(index) => target.insert(index, f(newElem))
+//              case NoLo => assert(false)
+//            }
+//            case Update(location, newElem, oldElem) => location match {
+//              case Start => target(0) = f(newElem)
+//              case End => target(target.size - 1) = f(newElem)
+//              case Index(index) => target(index) = f(newElem)
+//              case NoLo => assert(false)
+//            }
+//            case Reset => target.clear()
+//            case s: Script[A] => for (e <- s) process(pub, e)
+//          }
+//        }
+//  
+//        process(pub, change)
+//      }
+//    }
+//  }
+//  
+//  def liveCounted: Map[A, Int] with LiveMap[A, Int] = {
+//    val outer = this
+//    new HashMap[A, Int] with LiveMap[A, Int] {
+//      private final def target = this
+//      def attach(key: A): Unit = {
+//        val newCount = target.getOrElse(key, 0) + 1
+//        target(key) = newCount 
+//      }
+//      def release(key: A): Unit = {
+//        val oldCount = target.getOrElse(key, 0)
+//        if (oldCount <= 1) {
+//          target -= key
+//        } else {
+//          target += (key -> (oldCount - 1))
+//        }
+//      }
+//      val result2 = outer.changes.subscribeWeak { (pub: OldLiveSeq[A], change: Change[A]) =>
+//        def process(pub: OldLiveSeq[A], change: Change[A]): Unit = {
+//          change match {
+//            case Remove(_, oldElem) => release(oldElem)
+//            case Include(_, newElem) => attach(newElem)
+//            case Update(location, newElem, oldElem) => release(oldElem); attach(newElem)
+//            case Reset => target.clear()
+//            case s: Script[A] => for (e <- s) process(pub, e)
+//          }
+//        }
+//  
+//        process(pub, change)
+//      }
+//    }
+//  }
+//  
+//  def liveSet: Set[A] with LiveSet[A] = liveCounted.liveKeys
 }
