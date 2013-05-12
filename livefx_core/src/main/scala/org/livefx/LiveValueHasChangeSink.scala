a856b930a5839b5344153ef026679e757860ce64
John Ky <newhoggy@gmail.com>
2013-05-12 15:18:11 +1000

    Remove unused code.

--- livefx_core/src/main/scala/org/livefx/LiveValueHasChangeSink.scala
+++
@@ -1,9 +0,0 @@
-package org.livefx
-
-import org.livefx.script.Change
-
-trait LiveValueHasChangeSink[A] extends LiveValue[A] {
-  protected lazy val changesSink = new EventSource[Pub, Change[A]](publisher)
-  
-  def changes: Events[Pub, Change[A]] = changesSink
-}
