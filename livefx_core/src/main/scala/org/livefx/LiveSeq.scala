8bc25552e064fef597b0790e77a198e64575d0f0
John Ky <newhoggy@gmail.com>
2013-05-15 23:10:08 +1000

    Publish changes.

--- livefx_core/src/main/scala/org/livefx/LiveSeq.scala
+++ livefx_core/src/main/scala/org/livefx/LiveSeq.scala
@@ -4,8 +4,8 @@ import org.livefx.util.Memoize
 import org.livefx.trees.indexed.Tree
 import org.livefx.trees.indexed.Leaf
 import org.livefx.script._
-
 import scalaz.Monoid
+import org.livefx.script.Change
 
 trait LiveSeq[+A] extends Spoilable {
   type Pub <: LiveSeq[A]
@@ -18,10 +18,76 @@ trait LiveSeq[+A] extends Spoilable {
   
   def map[B](f: A => B): LiveSeq[B]
 
-  def +[B >: A](that: LiveSeq[B]): LiveSeq[B] = {
-    val self = this
+  def +[B >: A](bSeq: LiveSeq[B]): LiveSeq[B] = {
+    val aSeq = this
     new LiveSeqBinding[B] {
-      val ref = self.changes.subscribeWeak { (p, e) => Unit }
+      var aOldSize = aSeq.size.value
+      var bOldSize = bSeq.size.value
+
+      val aRef = aSeq.changes.subscribeWeak { (_, change) =>
+        change match {
+          case include@Include(location, newElem) => {
+            val translatedLocation = location match {
+              case start: Start => start
+              case end: End => end.asStart(aOldSize)
+              case NoLo => throw new IndexOutOfBoundsException
+            }
+            _changes.publish(include.copy(location = translatedLocation))
+            aOldSize += 1
+          }
+          case update@Update(location, newElem, oldElem) => {
+            val translatedLocation = location match {
+              case start: Start => start
+              case end: End => end.asStart(aOldSize)
+              case NoLo => throw new IndexOutOfBoundsException
+            }
+            _changes.publish(update.copy(location = translatedLocation))
+            // aOldSize no change
+          }
+          case remove@Remove(location, oldElem) => {
+            val translatedLocation = location match {
+              case start: Start => start
+              case end: End => end.asStart(aOldSize)
+              case NoLo => throw new IndexOutOfBoundsException
+            }
+            _changes.publish(remove.copy(location = translatedLocation))
+            aOldSize -= 1
+          }
+        }
+      }
+      
+      val bRef = bSeq.changes.subscribeWeak { (_, change) =>
+        change match {
+          case include@Include(location, newElem) => {
+            val translatedLocation = location match {
+              case start: Start => start.add(aOldSize)
+              case end: End => end.asStart(bOldSize).add(aOldSize)
+              case NoLo => throw new IndexOutOfBoundsException
+            }
+            _changes.publish(include.copy(location = translatedLocation))
+            bOldSize += 1
+          }
+          case update@Update(location, newElem, oldElem) => {
+            val translatedLocation = location match {
+              case start: Start => start.add(aOldSize)
+              case end: End => end.asStart(bOldSize).add(aOldSize)
+              case NoLo => throw new IndexOutOfBoundsException
+            }
+            _changes.publish(update.copy(location = translatedLocation))
+            // aOldSize no change
+          }
+          case remove@Remove(location, oldElem) => {
+            val translatedLocation = location match {
+              case start: Start => start.add(aOldSize)
+              case end: End => end.asStart(bOldSize).add(aOldSize)
+              case NoLo => throw new IndexOutOfBoundsException
+            }
+            _changes.publish(remove.copy(location = translatedLocation))
+            aOldSize -= 1
+          }
+        }
+      }
+      
       
       // TODO: Hook up events
       private lazy val _spoils = new EventSource[Pub, Spoil](publisher)
@@ -30,9 +96,9 @@ trait LiveSeq[+A] extends Spoilable {
       
       override def spoils: Events[Pub, Spoil] = _spoils
 
-      def size: LiveValue[Int] = for (selfSize <- self.size; thatSize <- that.size) yield selfSize + thatSize
+      def size: LiveValue[Int] = for (aSeqSize <- aSeq.size; thatSize <- bSeq.size) yield aSeqSize + thatSize
       
-      def map[C](f: B => C): LiveSeq[C] = self.map(f) + that.map(f)
+      def map[C](f: B => C): LiveSeq[C] = aSeq.map(f) + bSeq.map(f)
     }
   }
 }
--- livefx_core/src/main/scala/org/livefx/script/Location.scala
+++ livefx_core/src/main/scala/org/livefx/script/Location.scala
@@ -2,6 +2,14 @@ package org.livefx.script
 
 sealed abstract class Location
 
-case class Start(n: Int) extends Location
-case class End(n: Int) extends Location
+case class Start(index: Int) extends Location {
+  final def asEnd(hintSize: Int): End = End(hintSize - index - 1)
+  final def add(offset: Int): Start = Start(index + offset)
+}
+
+case class End(index: Int) extends Location {
+  final def asStart(hintSize: Int): Start = Start(hintSize - index - 1)
+  final def add(offset: Int): End = End(index + offset)
+}
+
 case object NoLo extends Location
