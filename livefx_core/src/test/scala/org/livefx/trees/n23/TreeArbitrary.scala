5348afd46e2e2cc13f74da0b7c1dddf79af855dd
John Ky <newhoggy@gmail.com>
2013-05-25 22:26:12 +1000

    Use a more structurally diverse arbitrary tree generator.

--- livefx_core/src/test/scala/org/livefx/trees/n23/TreeArbitrary.scala
+++ livefx_core/src/test/scala/org/livefx/trees/n23/TreeArbitrary.scala
@@ -3,8 +3,16 @@ package org.livefx.trees.n23
 import org.scalacheck.Arbitrary, Arbitrary._
 
 object TreeArbitrary {
-  implicit def ArbitraryTree[A](implicit A: Arbitrary[A]): Arbitrary[Tree[A]] =
+  def ArbitraryTree2[A](implicit A: Arbitrary[A]): Arbitrary[Tree[A]] =
     Arbitrary(arbitrary[List[A]] map {
       case (l) => Tree(l)
+    })
+
+  implicit def ArbitraryTree[A](implicit A: Arbitrary[A]): Arbitrary[Tree[A]] =
+    Arbitrary(arbitrary[List[(Int, A)]] map { l =>
+      l.foldLeft(Tree[A]()) { case (t, (n, v)) =>
+        val m = (n & 0x7ffffff) % (t.size + 1)
+        t.insertAt(m, v)
+      }
     })
 }
