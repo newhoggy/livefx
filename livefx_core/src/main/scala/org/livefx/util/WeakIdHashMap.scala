ee2845f8648df4c45c1732407a87c5cd75b9c93d
John Ky <newhoggy@gmail.com>
2013-05-27 20:34:23 +1000

    Remove unused code.

--- livefx_core/src/main/scala/org/livefx/util/WeakIdHashMap.scala
+++ livefx_core/src/main/scala/org/livefx/util/WeakIdHashMap.scala
@@ -22,10 +22,6 @@ class WeakIdHashMap[K, V]() extends MutMap[K, V] {
   private implicit val queue = new ReferenceQueue[K]
   private val backingStore = new HashMap[WeakIdRef[K], V]
 
-//  type Entry = DefaultEntry[K, V]
-//
-//  override def empty: HashMap[A, B] = reapThen(backingStore.isEmpty)
-
   override def clear(): Unit = {
     backingStore.clear()
     reap()
@@ -70,51 +66,6 @@ class WeakIdHashMap[K, V]() extends MutMap[K, V] {
     }
   }
 
-//
-//  override def foreach[C](f: ((K, V)) => C): Unit = foreachEntry(e => f(e.key, e.value))
-//
-//  override def keySet: scala.collection.Set[A] = new DefaultKeySet {
-//    override def foreach[C](f: A => C) = foreachEntry(e => f(e.key))
-//  }
-//
-//  /* Override to avoid tuple allocation in foreach */
-//  override def values: scala.collection.Iterable[V] = new DefaultValuesIterable {
-//    override def foreach[C](f: V => C) = foreachEntry(e => f(e.value))
-//  }
-//
-//  /* Override to avoid tuple allocation */
-//  override def keysIterator: Iterator[A] = new AbstractIterator[A] {
-//    val iter    = entriesIterator
-//    def hasNext = iter.hasNext
-//    def next()  = iter.next.key
-//  }
-//
-//  /* Override to avoid tuple allocation */
-//  override def valuesIterator: Iterator[V] = new AbstractIterator[V] {
-//    val iter    = entriesIterator
-//    def hasNext = iter.hasNext
-//    def next()  = iter.next.value
-//  }
-//
-//  def useSizeMap(t: Boolean) = if (t) {
-//    if (!isSizeMapDefined) sizeMapInitAndRebuild
-//  } else sizeMapDisable
-//
-//  protected def createNewEntry[B1](key: K, value: B1): Entry = {
-//    new Entry(key, value.asInstanceOf[V])
-//  }
-//
-//  private def writeObject(out: java.io.ObjectOutputStream) {
-//    serializeTo(out, { entry =>
-//      out.writeObject(entry.key)
-//      out.writeObject(entry.value)
-//    })
-//  }
-//
-//  private def readObject(in: java.io.ObjectInputStream) {
-//    init(in, createNewEntry(in.readObject().asInstanceOf[A], in.readObject()))
-//  }
-
   private final def reap(): Unit = this.synchronized {
     var zombie = queue.poll()
 
@@ -126,78 +77,4 @@ class WeakIdHashMap[K, V]() extends MutMap[K, V] {
   }
 
   private final def reapThen[T](f: => T): T = { reap(); f }
-
-//  override def + [V1 >: V] (kv: (K, V1)): Map[K, V1] = ???
-//  override def -(key: K): this.type = clone().asInstanceOf[this.type] -= key
 }
-
-//object HashMap extends MutableMapFactory[HashMap] {
-//  implicit def canBuildFrom[A, B]: CanBuildFrom[Coll, (A, B), HashMap[A, B]] = new MapCanBuildFrom[A, B]
-//  def empty[A, B]: HashMap[A, B] = new HashMap[A, B]
-//}
-//
-//
-//
-//
-//
-//class WeakIdHashMap[K, V] extends Map[K, V] {
-////  override def entrySet(): Set[Map.Entry[K, V]] = reapThen {
-////    val ret = new HashSet[Map.Entry[K, V]]
-////
-////    for (ref <- backingStore.entrySet()) {
-////      val key = ref.getKey.get
-////      val value = ref.getValue
-////      val entry = new Map.Entry[K, V] {
-////        override def getKey(): K = key
-////        override def getValue(): V = value
-////        override def setValue(value: V): V = throw new UnsupportedOperationException
-////      }
-////
-////      ret.add(entry)
-////    }
-////
-////    Collections.unmodifiableSet(ret)
-////  }
-//  
-//  override def keySet(): Set[K] = reapThen {
-//    val ret = new HashSet[K]
-//
-//    for (ref <- backingStore.keySet) {
-//        ret.add(ref.get)
-//    }
-//
-//    Collections.unmodifiableSet(ret)
-//  }
-//
-//  override def equals(any: Any): Boolean = {
-//    backingStore.equals(any.asInstanceOf[WeakIdHashMap[K, V]].backingStore)
-//  }
-//
-//  override def get(key: Any): V = reapThen {
-//    backingStore.get(WeakIdRef(key.asInstanceOf[K]))
-//  }
-//
-//  override def put(key: K, value: V): V = reapThen {
-//    backingStore.put(WeakIdRef(key), value)
-//  }
-//
-//  override def remove(key: Any): V  = reapThen {
-//    backingStore.remove(WeakIdRef(key.asInstanceOf[K]))
-//  }
-//  
-//  override def size(): Int = reapThen(backingStore.size)
-//
-//  override def values(): Collection[V] = reapThen(backingStore.values)
-//
-//  private final def reap(): Unit = this.synchronized {
-//    var zombie = queue.poll()
-//
-//    while (zombie != null) {
-//      val victim = zombie.asInstanceOf[WeakIdRef[K]]
-//      backingStore.remove(victim)
-//      zombie = queue.poll()
-//    }
-//  }
-//
-//  private final def reapThen[T](f: => T): T = { reap(); f }
-//}
