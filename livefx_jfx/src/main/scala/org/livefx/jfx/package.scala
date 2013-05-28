f6cbae774889085b9298df1a6458ff3cb02b0645
John Ky <newhoggy@gmail.com>
2013-05-28 22:08:36 +1000

    New asObservableSeq method.

--- livefx_jfx/src/main/scala/org/livefx/jfx/package.scala
+++ livefx_jfx/src/main/scala/org/livefx/jfx/package.scala
@@ -1,7 +1,18 @@
 package org.livefx
 
+import javafx.collections.ObservableList
+import javafx.collections.FXCollections
+import scala.collection.JavaConversions._
+
 package object jfx {
-  implicit class RichLiveVector[T](self: Live[Vector[T]]) {
-    def size: Live[Int] = ???
+  implicit class RichLiveIterable[T](self: Live[Iterable[T]]) {
+    def asObservableList: ObservableList[T] = {
+      val targetList = FXCollections.observableArrayList[T]()
+      val binding = for {
+        iterable <- self
+      } yield targetList.setAll(iterable.seq)
+      
+      return FXCollections.unmodifiableObservableList(targetList)
+    }
   }
 }
