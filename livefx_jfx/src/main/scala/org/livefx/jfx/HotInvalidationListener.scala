13aac6d6955debd4e6c32a2a2a221094b702952f
John Ky <newhoggy@gmail.com>
2013-05-29 23:43:55 +1000

    Remove name.

--- livefx_jfx/src/main/scala/org/livefx/jfx/HotInvalidationListener.scala
+++ livefx_jfx/src/main/scala/org/livefx/jfx/HotInvalidationListener.scala
@@ -5,12 +5,11 @@ import javafx.beans.Observable
 import javafx.beans.property.SimpleIntegerProperty
 import scala.collection.immutable.HashMap
 
-class HotInvalidationListener(val name: String) extends InvalidationListener {
+class HotInvalidationListener extends InvalidationListener {
   var properties = HashMap.empty[Any, Any]
 
   override def equals(o: Any): Boolean = o match {
     case that: HotInvalidationListener => {
-      println(s"$name is stealing properties from ${that.name}")
       properties = that.properties
       true
     }
@@ -22,15 +21,4 @@ class HotInvalidationListener(val name: String) extends InvalidationListener {
   override def invalidated(observable: Observable): Unit = Unit
 }
 
-object HotInvalidationListener {
-  def main(args: Array[String]): Unit = {
-    val property = new SimpleIntegerProperty(0)
-    val listener1 = new HotInvalidationListener("a")
-    val listener2 = new HotInvalidationListener("b")
-    property.addListener(listener1)
-    listener1.properties += ("Hello" -> "World")
-    property.removeListener(listener2)
-    property.addListener(listener2)
-    println(s"listener2.properties = ${listener2.properties}")
-  }
-}
+object HotInvalidationListener
