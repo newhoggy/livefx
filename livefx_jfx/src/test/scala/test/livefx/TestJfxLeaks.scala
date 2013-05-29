e09adfb8980dbe2c254f24eb09d83e6d4e47e432
John Ky <newhoggy@gmail.com>
2013-05-29 22:46:25 +1000

    Demonstrate JavaFX leak.

--- livefx_jfx/src/test/scala/test/livefx/TestJfxLeaks.scala
+++ livefx_jfx/src/test/scala/test/livefx/TestJfxLeaks.scala
@@ -9,46 +9,61 @@ import javafx.beans.WeakInvalidationListener
 import javafx.beans.property.SimpleIntegerProperty
 import scala.ref.WeakReference
 import scala.ref.ReferenceQueue
+import javafx.beans.binding.Bindings
+import javafx.beans.binding.BooleanBinding
 
 class TestJfxLeaks {
   @Test
   def testPropertyListenerLeaks(): Unit = {
-    val refQueue = new ReferenceQueue[WeakInvalidationListener]
     var listener = new InvalidationListener {
       override def invalidated(observable: Observable): Unit = Unit
     }
+    var weakListener = listener.weak
 
     val property = new SimpleIntegerProperty(0)
-    property.addListener(listener.weak: WeakInvalidationListener)
-    
-    val ref: Any = new WeakReference(listener, refQueue)
-    listener = null
-    
-    System.gc()
-    
-    Assert.assertTrue(refQueue.poll.isEmpty)
+    property.addListener(weakListener)
 
-    property.set(1)
-    
+    val refQueue1 = new ReferenceQueue[WeakInvalidationListener]
+    val refQueue2 = new ReferenceQueue[WeakInvalidationListener]
+    val ref1: Any = new WeakReference[InvalidationListener](listener, refQueue1)
+    val ref2: Any = new WeakReference[InvalidationListener](weakListener, refQueue2)
+    listener = null
+    weakListener = null
+
+    System.gc()
+    Thread.sleep(1)
+
+    // The invalidation listener is collected
+    Assert.assertTrue(refQueue1.poll.nonEmpty)
+    // But the weak invalidation listener still has a strong reference to it 
+    Assert.assertTrue(refQueue2.poll.isEmpty)
+
+    // At this point the weak invalidation gets an invalidation event and de-registers itself
+    property.set(99)
+
+    System.gc()
+    Thread.sleep(1)
+
+    Assert.assertTrue(refQueue1.poll.isEmpty)
+    // So the weak invalidation listener can be collected.
+    Assert.assertTrue(refQueue2.poll.nonEmpty)
+  }
+
+  @Test
+  def testPropertyBindingLeaks(): Unit = {
+    val property = new SimpleIntegerProperty(0)
+    var binding = Bindings.greaterThan(1, property);
+
+    val refQueue = new ReferenceQueue[BooleanBinding]
+    val ref: Any = new WeakReference[BooleanBinding](binding, refQueue)
+    binding = null
+
+    System.gc()
+    Thread.sleep(1)
+
+    // Binding is collected, along with its associated invalidation listener,
+    // but the weak invalidation listener reference is still strongly held
+    // representing a small leak.
     Assert.assertTrue(refQueue.poll.nonEmpty)
   }
-//
-//  @Test
-//  def testPropertyListenerAfterInvalidationLeakFree(): Unit = {
-//    val refQueue = new ReferenceQueue[WeakInvalidationListener]
-//    val property = new SimpleIntegerProperty(0)
-//    val listenerWeakRef = addListener(property, refQueue)
-//    
-//    System.gc()
-//    
-//    property.set(1)
-//    
-//    System.gc()
-//    
-//    
-//    listenerWeakRef match {
-//      case WeakReference(listener) => Assert.fail() // Invalidation should have unregistered listener
-//      case _ => // Invalidation unregistered listener as expected
-//    }
-//  }
 }
