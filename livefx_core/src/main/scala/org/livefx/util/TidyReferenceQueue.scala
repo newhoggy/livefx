2b5134e9ce8618e9176475ef900ea20ead7c7b1b
John Ky <newhoggy@gmail.com>
2013-06-01 00:56:36 +1000

    Iterate 10 times.

--- livefx_core/src/main/scala/org/livefx/util/TidyReferenceQueue.scala
+++ livefx_core/src/main/scala/org/livefx/util/TidyReferenceQueue.scala
@@ -3,10 +3,14 @@ package org.livefx.util
 import scala.ref.ReferenceQueue
 import scala.ref.WeakReference
 import scala.ref.Reference
+import scala.annotation.tailrec
 
 object TidyReferenceQueue extends ReferenceQueue[Nothing] {
-  def tidy(): Unit = poll match {
-    case Some(reference: TidyWeakReference[_]) => reference.dispose()
+  @tailrec
+  def tidy(count: Int = 10): Unit = poll match {
+    case Some(reference: TidyWeakReference[_]) =>
+      reference.dispose()
+      if (count >= 0) tidy(count - 1)
     case _ =>
   }
 
