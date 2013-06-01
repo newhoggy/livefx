161e460e711e3dafdfe7f0969f4c1d0ff532cb11
John Ky <newhoggy@gmail.com>
2013-06-01 11:13:33 +1000

    Experimentation with reflection.

--- livefx_jfx/src/main/scala/org/livefx/app/GenerateRiches.scala
+++ livefx_jfx/src/main/scala/org/livefx/app/GenerateRiches.scala
@@ -5,14 +5,47 @@ import javafx.scene.Node
 import javafx.beans.property.ObjectProperty
 import javafx.event.EventHandler
 import javafx.scene.input.KeyEvent
+import java.io.PrintStream
+import java.io.OutputStream
+import scala.util.DynamicVariable
+
+object Debug {
+  val originalOut = System.out
+  val suppressed = new DynamicVariable[Boolean](false)
+  val switchedOut = new PrintStream(new OutputStream() {
+    override def write(b: Int): Unit = {
+      if (!suppressed.value) {
+        originalOut.write(b)
+      }
+    }
+  })
+  System.setOut(switchedOut)
+  def suppressStdOut[A](f: => A): A = {
+    suppressed.withValue(true)(f)
+  }
+}
 
 object GenerateRiches {
   def main(args: Array[String]): Unit = {
     for (dec <- typeTag[Node].tpe.declarations) {
       if (dec.isMethod) {
-        val returnType = dec.asMethod.returnType 
-        if (returnType <:< typeOf[ObjectProperty[EventHandler[_ >: KeyEvent]]]) {
-          println(dec.asMethod.name + ": " + dec.asMethod.returnType)
+        val returnType = dec.asMethod.returnType
+        if (Debug.suppressStdOut(returnType <:< typeOf[ObjectProperty[_]])) {
+          returnType match {
+            case TypeRef(_, _, handlerType :: Nil) => {
+              handlerType match {
+                case ExistentialType(q::Nil, u) => {
+                  q.typeSignature match {
+                    case TypeBounds(lower, upper) => {
+                      println(s"--> $lower, $upper")
+                    }
+                  }
+                }
+                case _ =>
+              }
+            }
+            case _ =>
+          }
         }
       }
     }
