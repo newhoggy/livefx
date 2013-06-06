effd26f1027e4589e16ce0898f70bd5d8ab79ba8
John Ky <newhoggy@gmail.com>
2013-06-06 11:03:28 +1000

    Consolidate code generation.

--- livefx_jfx/src/main/scala/org/livefx/app/GenerateRiches.scala
+++ livefx_jfx/src/main/scala/org/livefx/app/GenerateRiches.scala
@@ -1,50 +1,139 @@
 package org.livefx.app
 
-import scala.reflect.runtime.universe._
-import javafx.scene.Node
+import scala.Option.option2Iterable
+import scala.collection.JavaConversions.asScalaSet
+import scala.reflect.runtime.universe
+import scala.reflect.runtime.universe.ExistentialType
+import scala.reflect.runtime.universe.TypeBounds
+import scala.reflect.runtime.universe.TypeRef
+import scala.reflect.runtime.universe.runtimeMirror
+import scala.reflect.runtime.universe.typeOf
+import org.livefx.util.EndLn
+import org.livefx.util.IndentWriter
+import org.reflections.Reflections
 import javafx.beans.property.ObjectProperty
-import javafx.event.EventHandler
-import javafx.scene.input.KeyEvent
-import java.io.PrintStream
-import java.io.OutputStream
-import scala.util.DynamicVariable
-import javafx.scene.control.ButtonBase
-import javafx.scene.control.Button
+import javafx.event.Event
+import javafx.scene.Node
+import javafx.scene.control.TableView
 import org.livefx.util.IO
 
 object GenerateRiches {
+  def subClassesOf[A](clazz: Class[A], packageName: String): List[Class[_ <: A]] = {
+    new Reflections("javafx.scene").getSubTypesOf(clazz).toList
+  }
+
+  def subClassesOf[A](clazz: Class[A]): List[Class[_ <: A]] = {
+    subClassesOf(clazz, clazz.getPackage().getName())
+  }
+
   def main(args: Array[String]): Unit = {
-    val nodeTag = typeTag[Node]
-    val m = runtimeMirror(classOf[Node].getClassLoader)
-    val pp = m.staticPackage("javafx.scene")
-    println(pp.typeSignature.declarations)
-    for (dec <- pp.typeSignature.declarations) {
-      println("==> " + dec)
+    val m = scala.reflect.runtime.currentMirror
+    val classes = classOf[Node]::subClassesOf(classOf[Node])
+    val classSymbols = classes.flatMap { clazz =>
+      try Some(m.classSymbol(clazz)) catch { case e: AssertionError => None }
     }
-    for (dec <- typeTag[ButtonBase].tpe.declarations) {
-      if (dec.isMethod) {
-        val method = dec.asMethod
-        val returnType = method.returnType
-        if (IO.suppressStdOut(returnType <:< typeOf[ObjectProperty[_]])) {
-          returnType match {
-            case TypeRef(_, _, handlerType :: Nil) => {
-              println(s"--> ${method.name}: ${handlerType}")
-              handlerType match {
-                case ExistentialType(q::Nil, u) => {
-                  q.typeSignature match {
-                    case TypeBounds(lower, upper) => {
-                      println(s"--> ${method.name} $lower, $upper")
+    val filteredClassSymbols = classSymbols.filter(!_.fullName.contains("$"))
+    val out = new IndentWriter(System.out)
+    val Regex = """on([a-zA-Z0-9_]+)Property""".r
+    out.println("package org.livefx.jfx")
+    out.println
+    out.println("import org.livefx.Events")
+    out.println
+    out.println("object Nodes {")
+    out.indent(2) {
+      out.println("object Implicits {")
+      out.indent(2) {
+        var firstClass = true
+        for (classSymbol <- filteredClassSymbols) {
+          if (!firstClass) out.println()
+          firstClass = false
+          val paramString = classSymbol.asType.typeParams match {
+            case Nil => ""
+            case ps => ps.map(x => x.name).mkString("[", ", ", "]")
+          }
+          out.println(s"implicit class Rich${classSymbol.name}$paramString(self: ${classSymbol.fullName}$paramString) {")
+          out.indent(2) {
+            for (classDeclaration <- classSymbol.typeSignature.declarations) {
+              if (classDeclaration.isMethod) {
+                val method = classDeclaration.asMethod
+                val returnType = method.returnType
+                if (IO.suppressStdOut(returnType <:< typeOf[ObjectProperty[_]])) {
+                  returnType match {
+                    case TypeRef(_, _, handlerType :: Nil) => {
+                      handlerType match {
+                        case ExistentialType(q::Nil, u) => {
+                          q.typeSignature match {
+                            case TypeBounds(lower, upper) =>
+                              if (lower <:< typeOf[Event]) {
+                                method.name.toString match {
+                                  case Regex(name) =>
+                                    if (name.length > 0) {
+                                      val normalName = name(0).toString.toLowerCase + name.substring(1)
+                                      out.println(s"def ${normalName}: Events[$lower] = {")
+                                      out.indent(2) {
+                                        out.println(s"val handler = self.${method.name}().getValue()")
+                                        out.println("if (handler == null) {")
+                                        out.indent(2) {
+                                          out.println(s"val events = new EventsWithEventHandler[$lower]")
+                                          out.println(s"self.${method.name}().setValue(events)")
+                                          out.println("events")
+                                        }
+                                        out.println("} else {")
+                                        out.indent(2) {
+                                          out.println("if (handler.isInstanceOf[EventsWithEventHandler[_]]) {")
+                                          out.indent(2) {
+                                            out.println(s"handler.asInstanceOf[EventsWithEventHandler[$lower]]")
+                                          }
+                                          out.println("} else {")
+                                          out.indent(2) {
+                                            out.println("throw new UnsupportedOperationException(\"event handler already installed\")")
+                                          }
+                                          out.println("}")
+                                        }
+                                        out.println("}")
+                                      }
+                                      out.println("}")
+                                    }
+                                  case _ =>
+                                  out.println(s"// skipped 0 ${method.name}")
+                                }
+                              } else {
+                                out.println(s"// skipped 1 ${method.name}")
+                              }
+                            case _ => 
+                              out.println(s"// skipped 2 ${method.name}")
+                          }
+                        }
+                        case TypeRef(_, _, eventType :: Nil) =>
+                          if (eventType <:< typeOf[Event]) {
+                            method.name.toString match {
+                              case Regex(name) =>
+                                if (name.length > 0) {
+                                  val normalName = name(0).toString.toLowerCase + name.substring(1)
+                                  out.println(s"def ${normalName}: Events[$eventType] = EventsWithEventHandler.on(self.${method.name}())")
+                                } else {
+                                  out.println(s"// skipped 3 ${method.name}")
+                                }
+                              case _ =>
+                              out.println(s"// skipped 4 ${method.name}")
+                            }
+                          } else {
+                            out.println(s"// skipped 5 ${method.name}")
+                          }
+                        case _ => out.println(s"// skipped 6 ${method.name}")
+                      }
                     }
+                    case _ =>
                   }
                 }
-                case TypeRef(_, _, eventType :: Nil) => println(s"--> ${method.name} $eventType")
-                case _ =>
               }
             }
-            case _ =>
           }
+          out.println("}")
         }
       }
+      out.println("}")
     }
+    out << "}" << EndLn
   }
 }
