e7c894610e2d7ecaae96ff5634017fae9875e2a3
John Ky <newhoggy@gmail.com>
2013-06-12 09:25:48 +1000

    New asEvents method.

--- livefx_core/src/main/scala/org/livefx/Events.scala
+++ livefx_core/src/main/scala/org/livefx/Events.scala
@@ -4,6 +4,7 @@ trait Events[+E] { self =>
   def subscribe(subscriber: E => Unit): Disposable
   def subscribeWeak(subscriber: E => Unit): Disposable
   def unsubscribe(subscriber: E => Unit): Unit
+  def asEvents: Events[E] = this
 
   def +[F >: E](that: Events[F]): Events[F] = new Events[F] {
     def subscribe(subscriber: F => Unit): Disposable = new Disposable {
