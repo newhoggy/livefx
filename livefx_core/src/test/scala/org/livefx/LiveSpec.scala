1952e026cec7906be9c24801ce8d7f964e6cd1ea
John Ky <newhoggy@gmail.com>
2013-06-16 19:22:31 +1000

    Should satisfy monad laws.

--- livefx_core/src/test/scala/org/livefx/LiveSpec.scala
+++ livefx_core/src/test/scala/org/livefx/LiveSpec.scala
@@ -10,12 +10,20 @@ import org.junit.runners.Suite
 
 class LiveSpec extends Spec {
   "Live" should {
-    "satisfy monad laws" ! monad.laws[Live]
+    "satisfy monad laws rightIdentity" ! monad.rightIdentity[Live, Int]
+    "satisfy monad laws leftIdentity" ! monad.leftIdentity[Live, Int, Int]
+    "satisfy monad laws associativity" ! monad.associativity[Live, Int, Int, Int]
+    "satisfy monad laws bindApConsistency" ! monad.bindApConsistency[Live, Int, Int]
   }
 
-  implicit def LiveIntEqual: Equal[Live[Int]] = Equal.equal((a, b) => a.value == b.value)
+  implicit def LiveIntEqual: Equal[Live[Int]] = Equal.equal { (a, b) =>
+    println(s"${a.value} <> ${b.value}")
+    new Exception().printStackTrace(System.out)
+    a.value == b.value
+  }
 }
 
 @RunWith(classOf[Suite])
 @Suite.SuiteClasses(Array(classOf[LiveSpec]))
 object LiveSpec extends LiveSpec
+
