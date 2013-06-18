d4cf1d472b0ad95334d4e192abdff0908197d42c
John Ky <newhoggy@gmail.com>
2013-06-18 20:00:26 +1000

    Use immutable HashSet rather than HashMap to reduce memory footprint.

--- livefx_core/src/main/scala/org/livefx/EventSource.scala
+++ livefx_core/src/main/scala/org/livefx/EventSource.scala
@@ -1,26 +1,37 @@
 package org.livefx
 
-import scala.collection.mutable.WeakHashMap
+import scala.collection.immutable.HashSet
+import org.livefx.util.TidyWeakReference
+import org.livefx.util.TidyReferenceQueue
 
 class EventSource[E] extends Events[E] with EventSink[E] {
-  private lazy val subscribers = new WeakHashMap[E => Unit, Option[E => Unit]]
+  private var subscribers = HashSet.empty[E => Unit]
 
   override def subscribe(subscriber: E => Unit): Disposable = new Disposable {
-    subscribers += (subscriber -> Some(subscriber))
-    
+    TidyReferenceQueue.tidy(1)
+
+    subscribers += subscriber
+
     override def dispose(): Unit = subscribers -= subscriber
   }
 
-  override def subscribeWeak(subscriber: E => Unit): Disposable = new Disposable {
-    subscribers += (subscriber -> None)
-    
-    override def dispose(): Unit = subscribers -= subscriber
+  override def subscribeWeak(subscriber: E => Unit): Disposable = {
+    TidyReferenceQueue.tidy(2)
+
+    val ref = new TidyWeakReference[E => Unit](subscriber, TidyReferenceQueue) with (E => Unit) {
+      override def dispose(): Unit = subscribers -= this
+
+      override def apply(event: E): Unit = this.get.map(s => s(event))
+    }
+
+    subscribers += ref
+    ref
   }
 
   override def unsubscribe(subscriber: E => Unit): Unit = { subscribers -= subscriber }
 
   override def publish(event: E): Unit = {
-    subscribers.foreach { entry => entry._1(event) }
+    subscribers.foreach { entry => entry(event) }
   }
 
   final def isEmpty: Boolean = subscribers.isEmpty
--- livefx_core/src/main/scala/org/livefx/util/TidyWeakReference.scala
+++ livefx_core/src/main/scala/org/livefx/util/TidyWeakReference.scala
@@ -2,7 +2,8 @@ package org.livefx.util
 
 import scala.ref.WeakReference
 import scala.ref.ReferenceQueue
+import org.livefx.Disposable
 
-abstract class TidyWeakReference[A <: AnyRef](a: A, queue: ReferenceQueue[A]) extends WeakReference[A](a, queue) {
+abstract class TidyWeakReference[A <: AnyRef](a: A, queue: ReferenceQueue[A]) extends WeakReference[A](a, queue) with Disposable {
   def dispose(): Unit
 }
