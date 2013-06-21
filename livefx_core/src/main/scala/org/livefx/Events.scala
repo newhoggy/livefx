5c09ad3ef1c1191dcbffee6590dadeac7263c8f0
John Ky <newhoggy@gmail.com>
2013-06-21 18:19:26 +1000

    Rename + to |.

--- livefx_core/src/main/scala/org/livefx/Events.scala
+++ livefx_core/src/main/scala/org/livefx/Events.scala
@@ -6,7 +6,7 @@ trait Events[+E] { self =>
   def subscribe(subscriber: E => Unit): Disposable
   def asEvents: Events[E] = this
 
-  def +[F >: E](that: Events[F]): Events[F] = new Events[F] {
+  def |[F >: E](that: Events[F]): Events[F] = new Events[F] {
     def subscribe(subscriber: F => Unit): Disposable = new Disposable {
       private var subscription1 = self.subscribe(subscriber)
       private var subscription2 = that.subscribe(subscriber)
@@ -16,6 +16,17 @@ trait Events[+E] { self =>
       } yield {
         subscription1 = null
         subscription2 = null
+      }
+    }
+  }
+
+  def map[F >: E](f: E => F): Events[F] = new Events[F] {
+    def subscribe(subscriber: F => Unit): Disposable = new Disposable {
+      private var subscription = self.subscribe(subscriber)
+      override protected def dispose(disposing: Boolean)(implicit ectx: ExecutionContext): Future[Unit] = for {
+        _ <- subscription.dispose()
+      } yield {
+        subscription = null
       }
     }
   }
