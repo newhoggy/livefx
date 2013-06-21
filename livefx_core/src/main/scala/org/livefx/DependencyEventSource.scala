c923519720429843271496ac49663be576f86ee0
John Ky <newhoggy@gmail.com>
2013-06-21 18:28:21 +1000

    Absorb EventSource code.

--- livefx_core/src/main/scala/org/livefx/DependencyEventSource.scala
+++ livefx_core/src/main/scala/org/livefx/DependencyEventSource.scala
@@ -1,9 +1,32 @@
 package org.livefx
 
+import scala.collection.immutable.HashSet
+import scala.concurrent._
 import org.livefx.util.TidyReferenceQueue
+import org.livefx.util.TidyWeakReference
 
-class DependencyEventSource[E] extends EventSource[E] {
-  override def publish(event: E): Unit = {
+class DependencyEventSource[E] extends Events[E] with EventSink[E] {
+  protected var subscribers = HashSet.empty[E => Unit]
+
+  override def subscribe(subscriber: E => Unit): Disposable = {
+    TidyReferenceQueue.tidy(2)
+
+    val ref = new TidyWeakReference[E => Unit](subscriber, TidyReferenceQueue) with (E => Unit) {
+      override protected def dispose(disposing: Boolean)(implicit ectx: ExecutionContext): Future[Unit] = {
+        subscribers -= this
+        future {}
+      }
+
+      override def apply(event: E): Unit = this.get.map(s => s(event))
+    }
+
+    subscribers += ref
+    ref
+  }
+
+  final def isEmpty: Boolean = subscribers.isEmpty
+
+  def publish(event: E): Unit = {
     TidyReferenceQueue.tidy(1)
     subscribers.foreach(s => s(event))
   }
