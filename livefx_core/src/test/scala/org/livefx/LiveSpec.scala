20b59d539106fde8c93d1fd7d0a835dda6a50c10
John Ky <newhoggy@gmail.com>
2013-06-23 11:46:55 +1000

    Remove unused code.

--- livefx_core/src/test/scala/org/livefx/LiveSpec.scala
+++ livefx_core/src/test/scala/org/livefx/LiveSpec.scala
@@ -11,19 +11,11 @@ import org.scalacheck.Arbitrary
 import org.scalacheck.Prop.forAll
 
 class LiveSpec extends Spec {
-  def apLikeDerived[M[_], A, B](fa: M[A], f: M[A => B])(implicit M: Monad[M], FB: Equal[M[B]]): Boolean = {
-    FB.equal(M.ap(fa)(f), M.bind(f)(f => M.map(fa)(f)))
-  }
-
-  def bindApConsistency[M[_], X, Y](implicit M: Monad[M], amx: Arbitrary[M[X]], af: Arbitrary[M[X => Y]], emy: Equal[M[Y]]) = {
-    forAll(apLikeDerived[M, X, Y] _)
-  }
-
   "Live" should {
     "satisfy monad laws rightIdentity" ! monad.rightIdentity[Live, Int]
     "satisfy monad laws leftIdentity" ! monad.leftIdentity[Live, Int, Int]
     "satisfy monad laws associativity" ! monad.associativity[Live, Int, Int, Int]
-    "satisfy monad laws bindApConsistency" ! bindApConsistency[Live, Int, Int]
+    "satisfy monad laws bindApConsistency" ! monad.bindApConsistency[Live, Int, Int]
   }
 
   implicit def LiveIntEqual: Equal[Live[Int]] = Equal.equal { (a, b) => a.value == b.value }
