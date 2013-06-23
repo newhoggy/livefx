dc8f7bb576bcf45e7a15298e55ef10478a907020
John Ky <newhoggy@gmail.com>
2013-06-23 18:46:42 +1000

    #1 implement and test map and flatMap.

--- livefx_core/src/main/scala/org/livefx/dependency/Events.scala
+++ livefx_core/src/main/scala/org/livefx/dependency/Events.scala
@@ -8,39 +8,42 @@ trait Events[+E] { self =>
   def subscribe(subscriber: E => Unit): Disposable
   def asEvents: Events[E] = this
 
-  def |[F >: E](that: Events[F]): Events[F] = new Events[F] {
-    def subscribe(subscriber: F => Unit): Disposable = new Disposable {
-      private var subscription1 = self.subscribe(subscriber)
-      private var subscription2 = that.subscribe(subscriber)
-      override protected def dispose(disposing: Boolean)(implicit ectx: ExecutionContext): Future[Unit] = for {
-        _ <- subscription1.dispose()
-        _ <- subscription2.dispose()
-      } yield {
-        subscription1 = null
-        subscription2 = null
-      }
+  def |[F >: E](that: Events[F]): Events[F] = new EventSource[F] with Disposable {
+    private var mapped = Option.empty[Disposable]
+    private var subscription1 = self.subscribe(publish)
+    private var subscription2 = that.subscribe(publish)
+
+    override protected def dispose(disposing: Boolean)(implicit ectx: ExecutionContext): Future[Unit] = for {
+      _ <- subscription1.dispose()
+      _ <- subscription2.dispose()
+    } yield {
+      subscription1 = null
+      subscription2 = null
     }
   }
 
-  def map[F >: E](f: E => F): Events[F] = new Events[F] {
-    def subscribe(subscriber: F => Unit): Disposable = new Disposable {
-      private var subscription = self.subscribe(subscriber)
-      override protected def dispose(disposing: Boolean)(implicit ectx: ExecutionContext): Future[Unit] = for {
-        _ <- subscription.dispose()
-      } yield {
-        subscription = null
-      }
+  def map[F >: E](f: E => F): Events[F] = new EventSource[F] with Disposable {
+    private var mapped = Option.empty[Disposable]
+    private var subscription = self.subscribe(e => publish(f(e)))
+
+    override protected def dispose(disposing: Boolean)(implicit ectx: ExecutionContext): Future[Unit] = for {
+      _ <- subscription.dispose()
+    } yield {
+      subscription = null
     }
   }
-  
-  /**
-   * flatMap
-   */
-  def flatMap[F](f: E => Events[F]): Events[F] = new EventSource[F] {
+
+  def flatMap[F](f: E => Events[F]): Events[F] = new EventSource[F] with Disposable {
     private var mapped = Option.empty[Disposable]
-    val ref = self.subscribe { e =>
+    private var subscription = self.subscribe { e =>
       mapped.foreach(_.dispose)
       mapped = Some(f(e).subscribe(publish))
+    }
+
+    override protected def dispose(disposing: Boolean)(implicit ectx: ExecutionContext): Future[Unit] = for {
+      _ <- subscription.dispose()
+    } yield {
+      subscription = null
     }
   }
 }
---
+++ livefx_core/src/test/scala/org/livefx/dependency/SpecEvents.scala
@@ -0,0 +1,46 @@
+package org.livefx.dependency
+
+import org.specs2.mutable.SpecificationWithJUnit
+
+class SpecEvents extends SpecificationWithJUnit {
+  "dependency.Events" should {
+    "should implement map" ! {
+      val es = new EventSource[Int]
+      val fs = es.map(_ + 1)
+      var esValues = List.empty[Int]
+      var fsValues = List.empty[Int]
+      es.subscribe(v => esValues ::= v)
+      fs.subscribe(v => fsValues ::= v)
+      es.publish(10)
+      esValues must_== List(10)
+      fsValues must_== List(11)
+      es.publish(20)
+      esValues must_== List(20, 10)
+      fsValues must_== List(21, 11)
+    }
+    "should implement flatMap" ! {
+      val bs = new EventSource[Boolean]
+      val ls = new EventSource[Int]
+      val rs = new EventSource[Int]
+      val zs = bs.flatMap(v => if (v) ls else rs)
+      var zsValues = List.empty[Int]
+      zs.subscribe(v => zsValues ::= v)
+      
+      ls.publish(0)
+      rs.publish(1)
+      bs.publish(false)
+      ls.publish(2)
+      rs.publish(3)
+      bs.publish(true)
+      ls.publish(4)
+      rs.publish(5)
+      bs.publish(false)
+      ls.publish(6)
+      rs.publish(7)
+      bs.publish(true)
+      ls.publish(8)
+      rs.publish(9)
+      zsValues must_== List(8, 7, 4, 3)
+    }
+  }
+}
