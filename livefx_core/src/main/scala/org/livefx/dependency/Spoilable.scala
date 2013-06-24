faa70dbc8ba559535132802020af83a05c89c1bd
John Ky <newhoggy@gmail.com>
2013-06-24 10:21:51 +1000

    Don't publish if not live.

--- livefx_core/src/main/scala/org/livefx/dependency/Spoilable.scala
+++ livefx_core/src/main/scala/org/livefx/dependency/Spoilable.scala
@@ -1,13 +1,16 @@
 package org.livefx.dependency
 
 import org.livefx.script.Spoil
+import org.livefx.Disposable
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
 
-trait Spoilable extends Publisher {
+trait Spoilable extends Publisher with Disposable {
   protected def spoilSink: EventSink[Spoil]
 
   def spoils: Events[Spoil]
 
   def spoiled: Boolean = false
 
-  protected def spoil(spoilEvent: Spoil): Unit = spoilSink.publish(spoilEvent)
+  protected def spoil(spoilEvent: Spoil): Unit = if (live) spoilSink.publish(spoilEvent)
 }
