e524ff3be785cceb80e8958f4ee8f3bd7eb2d334
John Ky <newhoggy@gmail.com>
2013-06-24 22:40:28 +1000

    Remove unused code.

--- livefx_core/src/main/scala/org/livefx/LeftOrRight.scala
+++
@@ -1,8 +0,0 @@
-package org.livefx
-
-object LeftOrRight {
-  def unapply[A](value: Either[A, A]): Option[A] = value match {
-    case scala.Left(x) => Some(x)
-    case scala.Right(x) => Some(x)
-  }
-}
