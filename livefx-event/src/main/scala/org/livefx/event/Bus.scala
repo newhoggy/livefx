74273f843071a92c1ee1091fc693857943373781
John Ky <newhoggy@gmail.com>
2016-06-05 14:33:04 +1000

    New ignore sink

---
+++ livefx-event/src/main/scala/org/livefx/event/Bus.scala
@@ -0,0 +1,5 @@
+package org.livefx.event
+
+object Bus {
+
+}
--- livefx-event/src/main/scala/org/livefx/event/Sink.scala
+++ livefx-event/src/main/scala/org/livefx/event/Sink.scala
@@ -3,3 +3,9 @@ package org.livefx.event
 trait Sink[-E] {
   def publish(event: E): Unit
 }
+
+object Sink {
+  val ignore = new Sink[Nothing] {
+    override def publish(event: Nothing): Unit = ()
+  }
+}
