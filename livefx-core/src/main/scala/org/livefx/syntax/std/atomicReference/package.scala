bca0a74645fd3fba0d6335aba21df509ace0d15c
John Ky <newhoggy@gmail.com>
2016-06-05 11:22:45 +1000

    New atomic reference syntax

---
+++ livefx-core/src/main/scala/org/livefx/syntax/std/atomicReference/package.scala
@@ -0,0 +1,24 @@
+package org.livefx.syntax.std
+
+import java.util.concurrent.atomic.AtomicReference
+
+import scala.annotation.tailrec
+
+package object atomicReference {
+  implicit class AtomicReferenceOps_YYKh2cf[A](val self: AtomicReference[A]) extends AnyVal {
+    def update(f: A => A): A = {
+      @tailrec
+      def go(oldValue: A): A = {
+        val currentValue = self.getAndSet(f(oldValue))
+
+        if (currentValue != oldValue) {
+          go(currentValue)
+        } else {
+          currentValue
+        }
+      }
+
+      go(self.get())
+    }
+  }
+}
