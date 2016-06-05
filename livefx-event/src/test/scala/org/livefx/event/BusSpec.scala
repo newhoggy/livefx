cb67e748d2bb97cfb3f08d2a66a4c959a2d2f25d
John Ky <newhoggy@gmail.com>
2016-06-05 23:52:42 +1000

    Remove commented out code

--- livefx-event/src/test/scala/org/livefx/event/BusSpec.scala
+++ livefx-event/src/test/scala/org/livefx/event/BusSpec.scala
@@ -22,31 +22,6 @@ class BusSpec extends Specification {
       fsValues must_== List(21, 11)
     }
 
-//    "should implement flatMap" ! {
-//      val bs = Bus[Boolean]
-//      val ls = Bus[Int]
-//      val rs = Bus[Int]
-//      val zs = bs.flatMap(v => if (v) ls else rs)
-//      var zsValues = List.empty[Int]
-//      zs.subscribe(v => zsValues ::= v)
-//
-//      ls.publish(0)
-//      rs.publish(1)
-//      bs.publish(false)
-//      ls.publish(2)
-//      rs.publish(3)
-//      bs.publish(true)
-//      ls.publish(4)
-//      rs.publish(5)
-//      bs.publish(false)
-//      ls.publish(6)
-//      rs.publish(7)
-//      bs.publish(true)
-//      ls.publish(8)
-//      rs.publish(9)
-//      zsValues must_== List(8, 7, 4, 3)
-//    }
-
     "should be able to mapConcat using function that produces an iterable" ! {
       val aBus = Bus[(Int, String)]
       val bBus = Bus[String]
@@ -119,19 +94,5 @@ class BusSpec extends Specification {
       rtBus.publish(3)
       result.value must_== List(3, 2, 1, 0)
     }
-
-//    "should implement impeded" ! {
-//      val es = Bus[Int]
-//      val esi = es.impeded
-//      var esiValues = List.empty[Int]
-//      esi.subscribe(v => esiValues ::= v)
-//      esiValues must_== List()
-//      es.publish(0)
-//      esiValues must_== List()
-//      es.publish(1)
-//      esiValues must_== List(0)
-//      es.publish(2)
-//      esiValues must_== List(1, 0)
-//    }
   }
 }
